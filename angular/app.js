11c0bfbffa871d7a8e8eb1a84994a0216b9587b1
garybuckle <buckle.g@mac.com>
2019-02-08 18:07:05 +0000

    up to ajax

--- angular/app.js
+++ angular/app.js
@@ -1,6 +1,14 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function(){
+myApp.controller('mainController', function($scope){
+    $scope.name ="Gazza";
+    $scope.occupation = "Athlete";
+    console.log($scope);
 
+    $scope.getName = function() {
+        return 'Gazza';
+    }
+
+    $scope.getName();
 });
 
