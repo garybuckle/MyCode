c654b18faba2f274b3653b8e90f5d79d23f9559f
garybuckle <buckle.g@mac.com>
2019-03-12 13:36:13 +0000

    added AngularJS

--- angular/app.js
+++ angular/app.js
@@ -1,14 +1,7 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function($scope){
-    $scope.name ="Gazza";
-    $scope.occupation = "Athlete";
-    console.log($scope);
-
-    $scope.getName = function() {
-        return 'Gazza';
-    }
+  
 
     $scope.getName();
 });
-
