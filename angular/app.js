cec17505d548bbc57f8930d27a81a3cd612657b6
garybuckle <buckle.g@mac.com>
2019-03-12 14:31:02 +0000

    more  angular

--- angular/app.js
+++ angular/app.js
@@ -1,9 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope){
-
-
-    $scope.getName();
+myApp.controller('mainController', function($scope, $log){
+  console.log($scope);
+  console.log($log);
 });
-
-function searchPeople(firstName,lastName,height,age)
