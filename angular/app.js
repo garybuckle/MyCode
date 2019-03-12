82e7771b1e871918883969c06b97a6afb9370377
garybuckle <buckle.g@mac.com>
2019-03-12 13:51:44 +0000

    angularJS learning maeterials

--- angular/app.js
+++ angular/app.js
@@ -1,7 +1,9 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function($scope){
-  
+
 
     $scope.getName();
 });
+
+function searchPeople(firstName,lastName,height,age)
